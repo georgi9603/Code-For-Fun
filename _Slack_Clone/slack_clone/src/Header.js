44294f7612d0b397c062cdecc6f86f400e6c0702
Георги <gtk960311@gmail.com>
2020-08-19 14:53:08 +0300

    installing material UI and adding header component

---
+++ _Slack_Clone/slack_clone/src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Avatar } from '@material-ui/core';
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <div className='header__left'>
+                <Avatar />
+
+            </div>
+            <div className='header__center'>
+
+            </div>
+            <div className='header__right'>
+
+            </div>
+        </div>
+    )
+}
