2f1696dcf45d6bd83721117f34b283fd311ff2e9
Георги <gtk960311@gmail.com>
2020-08-19 19:22:12 +0300

    Adding Side bar option component, icons and styles

--- _Slack_Clone/slack_clone/src/Sidebar.js
+++ _Slack_Clone/slack_clone/src/Sidebar.js
@@ -2,6 +2,16 @@ import React from 'react';
 import './Sidebar.css';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/Create';
+import SidebarOption from './SidebarOption';
+import InserCommentIcon from '@material-ui/icons/InsertComment';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 export default function Sidebar() {
     return (
@@ -16,6 +26,17 @@ export default function Sidebar() {
                 </div>
                 <CreateIcon />
             </div>
+            <SidebarOption Icon={InserCommentIcon} title="Threads" />
+            <SidebarOption title="Youtube" />
+            <SidebarOption Icon={InboxIcon} title="Mentions & reactions" />
+            <SidebarOption Icon={DraftsIcon} title="Saved items" />
+            <SidebarOption Icon={BookmarkBorderIcon} title="Channel browser" />
+            <SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+            <SidebarOption Icon={AppsIcon} title="Apps" />
+            <SidebarOption Icon={FileCopyIcon} title="File browser" />
+            <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+            <hr />
+            <SidebarOption Icon={ExpandMoreIcon} title="Show more" />
         </div>
     )
 }
