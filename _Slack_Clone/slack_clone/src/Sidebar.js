2a2895411445b5680ff6e8b364bf6b073ee6c03d
Георги <gtk960311@gmail.com>
2020-08-19 18:01:23 +0300

    Adding styles to sidebar header

--- _Slack_Clone/slack_clone/src/Sidebar.js
+++ _Slack_Clone/slack_clone/src/Sidebar.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Sidebar.css';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import CreateIcon from '@material-ui/icons/Create';
 
 export default function Sidebar() {
     return (
         <div className='sidebar'>
-
+            <div className='sidebar__header'>
+                <div className="sidebar__info">
+                    <h2>Slack</h2>
+                    <h3>
+                        <FiberManualRecordIcon />
+                    Georgi Karabozhilov
+                </h3>
+                </div>
+                <CreateIcon />
+            </div>
         </div>
     )
 }
