b97fd4edad8524f18e7753d61414d2dab3af3b9f
Георги <gtk960311@gmail.com>
2020-08-19 17:35:18 +0300

    Styling header

--- _Slack_Clone/slack_clone/src/Header.js
+++ _Slack_Clone/slack_clone/src/Header.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import './Header.css'
 import { Avatar } from '@material-ui/core';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline'
 
 export default function Header() {
     return (
         <div className='header'>
             <div className='header__left'>
-                <Avatar />
-
+                <Avatar className='header__avatar' alt='Georgi Karabozhilov' />
+                <AccessTimeIcon />
             </div>
             <div className='header__center'>
-
+                <SearchIcon />
+                <input placeholder='Search...'></input>
             </div>
             <div className='header__right'>
-
+                <HelpOutlineIcon />
             </div>
         </div>
     )
