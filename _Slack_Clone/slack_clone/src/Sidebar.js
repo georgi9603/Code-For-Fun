edf8838e94181c3fef2403f75b332562bb565c01
Георги <gtk960311@gmail.com>
2020-08-19 19:29:33 +0300

    Adding last icon

--- _Slack_Clone/slack_clone/src/Sidebar.js
+++ _Slack_Clone/slack_clone/src/Sidebar.js
@@ -12,6 +12,7 @@ import AppsIcon from '@material-ui/icons/Apps';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
 
 export default function Sidebar() {
     return (
@@ -36,7 +37,9 @@ export default function Sidebar() {
             <SidebarOption Icon={FileCopyIcon} title="File browser" />
             <SidebarOption Icon={ExpandLessIcon} title="Show less" />
             <hr />
-            <SidebarOption Icon={ExpandMoreIcon} title="Show more" />
+            <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+            <hr />
+            <SidebarOption Icon={AddIcon} title="Add Channel" />
         </div>
     )
 }
