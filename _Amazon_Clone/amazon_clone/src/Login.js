f024b7a5d02b91bf3c729c0a59ca4f8e4f116871
Георги <gtk960311@gmail.com>
2020-08-23 19:30:50 +0300

    Adding login page and styling it

--- _Amazon_Clone/amazon_clone/src/Login.js
+++ _Amazon_Clone/amazon_clone/src/Login.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Login.css';
+import { Link, useHistory } from 'react-router-dom';
+import { auth } from './firebase';
 
 function Login() {
+    const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault();
+
+        auth.signInWithEmailAndPassword(email, password)
+            .then((auth) => {
+                history.push("/")
+            })
+            .catch(e => alert(e.message));
+    }
+
+    const createAccount = e => {
+        e.preventDefault();
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                history.push("/")
+            })
+            .catch(e => alert(e.message));
+    }
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="login">
+            <Link to="/">
+                <img
+                    className="login__logo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+                    alt=""
+                />
+            </Link>
+            <div className="login__container">
+                <h1>Sign In</h1>
+                <form>
+                    <h5>Username</h5>
+                    <input value={email} onChange={event => setEmail(event.target.value)} type="email" />
+                    <h5>Password</h5>
+                    <input value={password} onChange={event => setPassword(event.target.value)} type="password" />
+                    <button onClick={signIn} type="submit">Sign In</button>
+                </form>
+                <p>
+                    By signing-in you agree to Amazon's Conditions of Use & Sale.
+                    Please see our Privacy Notice, our Cokkies Notice and our interest-Based Ads Notice.
+                </p>
+                <button onClick={createAccount}>Create your Amazon Account</button>
+            </div>
         </div>
     )
 }
--- _Amazon_Clone/amazon_clone/src/firebase.js
+++ _Amazon_Clone/amazon_clone/src/firebase.js
@@ -1,3 +1,7 @@
-const firebaseConfig = {
+import firebase from "firebase";
 
-};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+export { auth };
