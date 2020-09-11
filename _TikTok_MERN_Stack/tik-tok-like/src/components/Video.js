df77e86b931dbb0834a74b844b4b9b7cbe6b39d5
Георги <gtk960311@gmail.com>
2020-09-11 22:24:05 +0300

    Adding props through app video and sidebar

--- _TikTok_MERN_Stack/tik-tok-like/src/components/Video.js
+++ _TikTok_MERN_Stack/tik-tok-like/src/components/Video.js
@@ -6,7 +6,7 @@ import './Video.css';
 import VideoFooter from './VideoFooter';
 import VideoSidebar from './VideoSidebar';
 
-function Video() {
+function Video({ videoURL, likes, messages, shares }) {
     const videoRef = useRef(null)
     const [shoutPlay, setShouldPlay] = useState(false)
 
@@ -27,12 +27,18 @@ function Video() {
                 onClick={handleClickOnVideo}
                 loop
                 ref={videoRef}
-                src="https://v16m.tiktokcdn.com/6f2eb2ae7ef2c2749f3078bf9bb1fbaf/5f5ab2f2/video/tos/alisg/tos-alisg-pv-0037/51dde7afaa8e4569bf9bf8d89deebd6b/?a=1180&amp;br=7472&amp;bt=3736&amp;cr=0&amp;cs=0&amp;cv=1&amp;dr=3&amp;ds=6&amp;er=&amp;l=20200910171240010190219222440705A0&amp;lr=tiktok&amp;mime_type=video_mp4&amp;qs=13&amp;rc=amc4ZjtweG11dzMzOzgzM0ApZnV2aTg2aXU7ZTMzNDU6eWdgb3BjMjBjbzVfLS0uLzRzc2RhMmA1anJnb3EtLTUyLS06Yw%3D%3D&amp;vl=&amp;vr="
+                src={videoURL}
             >
             </video>
 
-            <VideoFooter />
-            <VideoSidebar />
+            <VideoFooter
+
+            />
+            <VideoSidebar
+                likes={likes}
+                messages={messages}
+                shares={shares}
+            />
         </div>
     )
 }
--- _TikTok_MERN_Stack/tik-tok-like/src/components/VideoSidebar.js
+++ _TikTok_MERN_Stack/tik-tok-like/src/components/VideoSidebar.js
@@ -4,20 +4,20 @@ import MessageIcon from '@material-ui/icons/Message';
 import ShareIcon from '@material-ui/icons/Share';
 import './VideoSidebar.css';
 
-function VideoSidebar() {
+function VideoSidebar({ likes, messages, shares }) {
     return (
         <div className="videoSidebar">
             <div className="videoSidebar__button">
                 <FavoriteIcon className="videoSidebar__buttonLike" fontSize="large" />
-                <p>1000</p>
+                <p>{likes}</p>
             </div>
             <div className="videoSidebar__button">
                 <MessageIcon className="videoSidebar__buttonMessage" fontSize="large" />
-                <p>250</p>
+                <p>{messages}</p>
             </div>
             <div className="videoSidebar__button">
                 <ShareIcon className="videoSidebar__buttonShare" fontSize="large" />
-                <p>20</p>
+                <p>{shares}</p>
             </div>
         </div>
     )
