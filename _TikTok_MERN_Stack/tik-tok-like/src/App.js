6cbadc5050b30498dcb9ae7034a958a0f15ff333
Георги <gtk960311@gmail.com>
2020-09-11 22:48:16 +0300

    Changeing video url and adding is liked functionality

--- _TikTok_MERN_Stack/tik-tok-like/src/App.js
+++ _TikTok_MERN_Stack/tik-tok-like/src/App.js
@@ -7,12 +7,17 @@ function App() {
     <div className="app">
       <div className="app__videos">
         <Video
-          videoURL="https://v16m.tiktokcdn.com/6f2eb2ae7ef2c2749f3078bf9bb1fbaf/5f5ab2f2/video/tos/alisg/tos-alisg-pv-0037/51dde7afaa8e4569bf9bf8d89deebd6b/?a=1180&amp;br=7472&amp;bt=3736&amp;cr=0&amp;cs=0&amp;cv=1&amp;dr=3&amp;ds=6&amp;er=&amp;l=20200910171240010190219222440705A0&amp;lr=tiktok&amp;mime_type=video_mp4&amp;qs=13&amp;rc=amc4ZjtweG11dzMzOzgzM0ApZnV2aTg2aXU7ZTMzNDU6eWdgb3BjMjBjbzVfLS0uLzRzc2RhMmA1anJnb3EtLTUyLS06Yw%3D%3D&amp;vl=&amp;vr="
+          videoURL="https://v77.tiktokcdn.com/2e7dde31028986e2c749802c18611cda/5f5c2656/video/tos/useast2a/tos-useast2a-ve-0068c003/a29b7dda161f489e8c1f18317fa6c581/?a=1233&br=2836&bt=1418&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202009111937170101880612031B003D27&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ajxka3A7czd4dTMzZzczM0ApNzs4NjQ6ZzxnNzxmaDhmOWdwMC5sMjM1MS9fLS02MTZzcy0vNDI2MzNeLzY0Y19gL2E6Yw%3D%3D&vl=&vr="
           likes={1000}
           messages={420}
           shares={20}
         />
-        <Video />
+        <Video
+          videoURL="https://v77.tiktokcdn.com/2e7dde31028986e2c749802c18611cda/5f5c2656/video/tos/useast2a/tos-useast2a-ve-0068c003/a29b7dda161f489e8c1f18317fa6c581/?a=1233&br=2836&bt=1418&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202009111937170101880612031B003D27&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ajxka3A7czd4dTMzZzczM0ApNzs4NjQ6ZzxnNzxmaDhmOWdwMC5sMjM1MS9fLS02MTZzcy0vNDI2MzNeLzY0Y19gL2E6Yw%3D%3D&vl=&vr="
+          likes={1000}
+          messages={420}
+          shares={20}
+        />
       </div>
     </div>
   );
--- _TikTok_MERN_Stack/tik-tok-like/src/components/VideoSidebar.js
+++ _TikTok_MERN_Stack/tik-tok-like/src/components/VideoSidebar.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import MessageIcon from '@material-ui/icons/Message';
 import ShareIcon from '@material-ui/icons/Share';
 import './VideoSidebar.css';
 
 function VideoSidebar({ likes, messages, shares }) {
+    const [isLiked, setIsLiked] = useState(false);
+
+    const handleLike = () => {
+        setIsLiked(!isLiked)
+    }
+
     return (
         <div className="videoSidebar">
-            <div className="videoSidebar__button">
-                <FavoriteIcon className="videoSidebar__buttonLike" fontSize="large" />
+            {console.log(isLiked)}
+            <div onClick={handleLike} className="videoSidebar__button">
+                <FavoriteIcon className={isLiked ? "videoSidebar__liked" : "videoSidebar__buttonLike"} fontSize="large" />
                 <p>{likes}</p>
             </div>
             <div className="videoSidebar__button">
