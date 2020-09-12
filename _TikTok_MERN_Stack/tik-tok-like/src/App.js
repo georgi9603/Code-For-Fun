4c12721495b59bbc5054ecc2622ae4e1a883475d
Георги <gtk960311@gmail.com>
2020-09-12 13:29:27 +0300

    Adding footer, ticker and img

--- _TikTok_MERN_Stack/tik-tok-like/src/App.js
+++ _TikTok_MERN_Stack/tik-tok-like/src/App.js
@@ -7,13 +7,13 @@ function App() {
     <div className="app">
       <div className="app__videos">
         <Video
-          videoURL="https://v77.tiktokcdn.com/2e7dde31028986e2c749802c18611cda/5f5c2656/video/tos/useast2a/tos-useast2a-ve-0068c003/a29b7dda161f489e8c1f18317fa6c581/?a=1233&br=2836&bt=1418&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202009111937170101880612031B003D27&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ajxka3A7czd4dTMzZzczM0ApNzs4NjQ6ZzxnNzxmaDhmOWdwMC5sMjM1MS9fLS02MTZzcy0vNDI2MzNeLzY0Y19gL2E6Yw%3D%3D&vl=&vr="
+          videoURL="https://v77.tiktokcdn.com/698dd8cd4bd7a8ddeab458e71df69b0a/5f5ce905/video/tos/useast2a/tos-useast2a-ve-0068c001/f4dcd86e7454477b9e31f4b38621ddc8/?a=1233&br=2412&bt=1206&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20200912092755010189071149480F504D&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=anlncXRvZ3ZudjMzZjczM0ApODZoaTY8Nzw3N2ZnNjk7ZWdvbDNiLXJuMWZfLS02MTZzczRgYS0tXi42XzYuXzFgNGE6Yw%3D%3D&vl=&vr="
           likes={1000}
           messages={420}
           shares={20}
         />
         <Video
-          videoURL="https://v77.tiktokcdn.com/2e7dde31028986e2c749802c18611cda/5f5c2656/video/tos/useast2a/tos-useast2a-ve-0068c003/a29b7dda161f489e8c1f18317fa6c581/?a=1233&br=2836&bt=1418&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202009111937170101880612031B003D27&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ajxka3A7czd4dTMzZzczM0ApNzs4NjQ6ZzxnNzxmaDhmOWdwMC5sMjM1MS9fLS02MTZzcy0vNDI2MzNeLzY0Y19gL2E6Yw%3D%3D&vl=&vr="
+          videoURL="https://v77.tiktokcdn.com/25438f70e2115fe3d4e40e13f7962222/5f5ce958/video/tos/useast2a/tos-useast2a-ve-0068c001/1a3678d24d7b45a6969d6d6c235a81a8/?a=1233&br=2728&bt=1364&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20200912092921010190218203510F0E25&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M2Y6ZWQ8aHVtdzMzaTczM0ApOzg3ZjU4OzxkNzU0NDM1ZGdsZW1xXmRgbjZfLS1gMTZzc2M2YzJhYjQ1MzA1YzY0X146Yw%3D%3D&vl=&vr="
           likes={1000}
           messages={420}
           shares={20}
--- _TikTok_MERN_Stack/tik-tok-like/src/components/VideoFooter.js
+++ _TikTok_MERN_Stack/tik-tok-like/src/components/VideoFooter.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import './VideoFooter.css'
+import Ticker from 'react-ticker';
+import MusicIcon from '@material-ui/icons/MusicNote'
+
+
+import './VideoFooter.css';
+
 
 function VideoFooter() {
     return (
-        <div>
-
+        <div className="videoFooter">
+            <div className="videoFooter__text">
+                <h3>@lotterlive</h3>
+                <p>Description</p>
+                <div className="videoFooter__ticker">
+                    <MusicIcon className="videoFooter__icon" />
+                    <Ticker mode="smooth">
+                        {({ index }) => (
+                            <>
+                                <p>This is the Headline of element #{index}!</p>
+                            </>
+                        )}
+                    </Ticker>
+                </div>
+            </div>
+            <img
+                className="videoFooter__recordAnimation"
+                src="https://static.thenounproject.com/png/934821-200.png"
+                alt=""
+            >
+            </img>
         </div>
     )
 }
