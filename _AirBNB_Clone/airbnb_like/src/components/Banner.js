3f8a03fd148027b8b9d813b834acb42346256bd7
Георги <gtk960311@gmail.com>
2020-09-16 22:20:02 +0300

    Adding calender with react date picker

--- _AirBNB_Clone/airbnb_like/src/components/Banner.js
+++ _AirBNB_Clone/airbnb_like/src/components/Banner.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from './Calendar';
 import { Button } from '@material-ui/core';
 
 import './Banner.css'
 
 function Banner() {
+    const [isCalendarOpen, setisCalendarOpen] = useState(false)
+
     return (
         <div className="banner">
-            <div className="banner__info">
+            <div className="banner__dateSearch">
+                <Button
+                    onClick={() => setisCalendarOpen(!isCalendarOpen)}
+                    className="banner__dateSearchButton"
+                    variant="outlined"
+                >
+                    {isCalendarOpen ? " Hide" : "Show"} calendar
+                </Button>
+                {isCalendarOpen && <Calendar />}
+            </div>
+            {!isCalendarOpen && <div className="banner__info">
                 <h1>Get out and strech your imagination</h1>
                 <h4>Wherever you go, go with all your heart</h4>
                 <Button>Explore nearby</Button>
-            </div>
+            </div>}
         </div>
     )
 }
