a99f6985998892c199b39e7a066511472551b31d
Георги <gtk960311@gmail.com>
2020-09-17 23:42:46 +0300

    New component

---
+++ _AirBNB_Clone/airbnb_like/src/components/propertyCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropertyCard from './propertyCard';
+
+function propertyCard() {
+    return (
+        <div className="propertyCard">
+
+        </div>
+    )
+}
+
+export default propertyCard
